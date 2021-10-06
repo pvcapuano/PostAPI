fb8af981324b56c6f9b41d0e544330aa9838270d
Paulo Victor Capuano <pvcapuano@gmail.com>
2021-10-06 19:03:30 +0000

    db config

--- API/src/server.js
+++ API/src/server.js
@@ -1,5 +1,14 @@
 const express = require("express")
 const routes = require("./routes")
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb+srv://pvcapuano:123mudar@cluster0.ksdnl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{ useNewUrlParser: true,
+useUnifiedTopology: true,
+})
+
+const db = mongoose.connection
+db.on("error", (error) => console.error(error))
+db.once("open", () => console.log("conectado ao DB"))
 
 const app = express()
 const port = 3333
